a03686474a529227568a91aea6aa6d77a9faaff3
Putu Hery <iphery@gmail.com>
2024-08-31 07:06:06 +0800

    1

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,19 +7,20 @@ export default function Page() {
   const [user, setUser] = useState(null);
   const router = useRouter();
 
+  const checkAuth = async () => {
+    const res = await fetch(`${API_PINEAPPLE}/protected`, {
+      credentials: "include",
+    });
+    console.log("Cookies after request:", document.cookie);
+    if (res.ok) {
+      const data = await res.json();
+      setUser(data.user);
+    } else {
+      router.push("/login");
+    }
+  };
+
   useEffect(() => {
-    const checkAuth = async () => {
-      const res = await fetch(`${API_PINEAPPLE}/protected`, {
-        credentials: "include",
-      });
-      console.log("Cookies after request:", document.cookie);
-      if (res.ok) {
-        const data = await res.json();
-        setUser(data.user);
-      } else {
-        router.push("/login");
-      }
-    };
     checkAuth();
   }, []);
 
